11e436dac275b6e15ac4dc5d99513cc45dd3bc28
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-12-08 20:16:01 -0300

    capturo la primera historia y la guardo

--- index.spec.js
+++ index.spec.js
@@ -6,11 +6,12 @@ dotenv.config();
 const accounts = JSON.parse(process.env.INSTAGRAM_ACCOUNTS);
 
 test('Scraping of Instagram counts', async ({ page }) => {
+  test.setTimeout(120000);
   // Inicio de sesión
   await page.goto('https://www.instagram.com/');
   await page.waitForSelector('#loginForm');
-  await page.locator('input[name="username"]').pressSequentially(process.env.INSTAGRAM_USERNAME, { delay: 200 });
-  await page.locator('input[name="password"]').pressSequentially(process.env.INSTAGRAM_PASSWORD, { delay: 200 });
+  await page.locator('input[name="username"]').pressSequentially(process.env.INSTAGRAM_USERNAME, { delay: 20 });
+  await page.locator('input[name="password"]').pressSequentially(process.env.INSTAGRAM_PASSWORD, { delay: 20 });
   await page.click('button[type="submit"]');
   // Valido mediante una notificación que se haya iniciado sesión correctamente
   await page.waitForSelector('main > div > div > div > section > div > button');
@@ -29,13 +30,24 @@ test('Scraping of Instagram counts', async ({ page }) => {
       await page.waitForSelector('svg[aria-label="Menú"]');
       // Cuento la cantidad de historias
       const count = await page.locator('section > div > div > div > div > div > div:nth-child(1) > div > div:nth-child(1) > div').count();
-      await page.waitForTimeout(1000); // Espera de 1 segundo antes de clickear la flecha de anterior
+      await page.waitForTimeout(100);
 
       // Si figura la flecha de anterior, la clickeo varias veces hasta llegar a la primera historia
       while (await page.locator('svg[aria-label="Anterior"]').count() > 0) {
         await page.locator('svg[aria-label="Anterior"]').click();
-        await page.waitForTimeout(1000); // Espera de 1 segundo entre clics
+        await page.waitForTimeout(100); 
       }
+
+      // Valido que se haya cargado el boton de pausar
+      await page.waitForSelector('svg[aria-label=Pausar]');
+      page.locator('svg[aria-label=Pausar]').click();
+      
+      // Valido que se haya cargado la imagen o video de la primera historia
+      await page.waitForSelector('section > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > div > div > img, section > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div > div > div > div > video'); 
+
+      // Hago screenshot de la primera historia y la guardo en la carpeta screenshots
+      let storyNumber = 1;
+      await page.locator('section > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > div > div > img, section > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div > div > div > div > video').screenshot({ path: `screenshots/${account.username}_${storyNumber}_${new Date().toLocaleDateString().replace(/\//g, '')}.png` });
       
       console.log('Account:', account.username);
       console.log(`Has ${count} stories`);
