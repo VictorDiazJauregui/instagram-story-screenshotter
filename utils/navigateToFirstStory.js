50224c3f09c3a5b5df068ebd22984d50a89299fb
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-12-09 23:24:33 -0300

    navegado a la primera historia, modularizado

---
+++ utils/navigateToFirstStory.js
@@ -0,0 +1,8 @@
+export const navigateToFirstStory = async (page) => {
+  while (await page.locator('svg[aria-label="Anterior"]').count() > 0) {
+    await page.locator('svg[aria-label="Anterior"]').click();
+    await page.waitForTimeout(100);
+  }
+  await page.waitForSelector('svg[aria-label=Pausar]');
+  page.locator('svg[aria-label=Pausar]').click();
+};
