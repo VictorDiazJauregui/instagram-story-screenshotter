5b379ca6823dff1fc3afcc8c0acc2470b20b467a
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-12-09 23:25:29 -0300

    screenshot modularizado

---
+++ utils/takeScreenshot.js
@@ -0,0 +1,11 @@
+export const takeScreenshot = async (page, account, storyNumber) => {
+  const path = 'section > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > div > div';
+  const pathImg = `${path} > img`;
+  const pathVideo = `${path} > div > div > div > div > div > video`;
+  await page.waitForSelector(`${pathImg}, ${pathVideo}`);
+  await page.waitForTimeout(500);
+
+  const datePath = new Date().toLocaleDateString().replace(/\//g, '');
+  const screenshotPath = `screenshots/${account.username}_${storyNumber}_${datePath}.png`;
+  await page.locator(`${pathImg}, ${pathVideo}`).screenshot({ path: screenshotPath });
+};
