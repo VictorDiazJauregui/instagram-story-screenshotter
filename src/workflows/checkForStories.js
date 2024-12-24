01f19cf32210e3440c7560814ac7a0474d80789e
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-12-24 15:20:14 -0300

    Arreglo de los imports en los workflows

--- src/workflows/checkForStories.js
+++ src/workflows/checkForStories.js
@@ -1,4 +1,4 @@
-import { profilePicture } from '../src/utils/selectors.js';
+import { profilePicture } from '../utils/selectors.js';
 
 export const checkForStories = async (page) => {
   return (await page.locator(profilePicture).count()) > 0;
